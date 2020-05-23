3515bea39bad51424b43a55bac61d8b145dc7667
paulsjohnson91 <paulsjohnson91@users.noreply.github.com>
2020-05-23 17:34:53 +0000

    deploy: 6c2b35e534aacffca75c6c9f5f29eecb18f5473a

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7def81f1647728eb6a0c501e40fd85c7.js"
+  "/precache-manifest.c972c0ba0d015a1f2ec07183700f5661.js"
 );
 
 self.addEventListener('message', (event) => {
