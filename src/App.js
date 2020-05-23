a496c1b1b33882d022b72ee4366a8d71c7e48919
psj03 <paul.johnson2@sky.uk>
2020-05-23 19:57:53 +0100

    fix auth

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import {Layout} from 'antd';
 import {MyHeader} from "./components/layout/header";
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route, useLocation} from 'react-router-dom';
 import {SearchScreenDefault} from "./screens/search/search-screen-default";
 import {AddRssFeedScreen} from "./screens/add-rss-feed";
 import {AuthScreen} from "./screens/auth/auth-screen";
@@ -12,7 +12,6 @@ import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 // AWS Config Files
 import awsConfig from './configs/aws-configs'
 import UserProfile from "./screens/profile/UserProfile";
-// import image from "*.jpg";
 import routes from "./routes.js";
 import ViewChallenges from "./screens/viewchallenges/ViewChallenges";
 import CreateChallenge from "./screens/challenge/create/CreateChallenge";
@@ -54,6 +53,7 @@ export class App extends React.Component {
     });
   };
 
+
   componentDidUpdate(e) {
     if (
         window.innerWidth < 993 &&
@@ -69,15 +69,16 @@ export class App extends React.Component {
     }
   }
 
+
+
   render() {
     return (
         <div className="wrapper">
 
           <div id="main-panel" className="main-panel" ref="mainPanel">
             <Router>
-              <Route path={"/"}
-                     render={props => <Sidebar {...props} routes={routes}
-                                               color={this.state.color}/>}/>
+              <Route path={"/"} render={props => <Sidebar {...props} routes={routes}
+                                                          color={this.state.color}/>} />
 
               <Layout>
 
@@ -103,11 +104,9 @@ export class App extends React.Component {
                       <Route path={"/forgotpassword1"} component={AuthScreen}/>
                       <Route path={"/forgotpassword2"} component={AuthScreen}/>
                       <Route path={"/profile"} component={UserProfile}/>
-                      <Route path={"/challenges/view"}
-                             component={ViewChallenges}/>
+                      <Route path={"/challenges/view"} component={ViewChallenges}/>
                       {/*<Route path={"/challenges/create"} component={CreateChallenge}/>*/}
-                      <Route path={"/challenges/create/:id?"}
-                             component={CreateChallenge}/>
+                      <Route path={"/challenges/create/:id?"} component={CreateChallenge}/>
                       <Route path={"/homepage"} component={HomePage}/>
                       <Route path={"/carousel"} component={Carousel}/>
                       <Route path={"/bugs"} component={Bugs}/>
