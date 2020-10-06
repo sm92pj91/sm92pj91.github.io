b2a3928488d9742db414be97ff5de25c5fb34380
paulsjohnson91 <paulsjohnson91@users.noreply.github.com>
2020-10-06 14:25:35 +0000

    deploy: d0237b8ee625fe7e2ccccfea80f6e2562e552aa2

---
+++ precache-manifest.4f048a12fbfecd1ed82e88ba41f9c640.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c775180714edc56e9a8b02116e1e731e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6326456ddae77699482",
+    "url": "/static/css/2.3cf09a79.chunk.css"
+  },
+  {
+    "revision": "c81bed8303fd1206973a",
+    "url": "/static/css/main.2b751f89.chunk.css"
+  },
+  {
+    "revision": "c6326456ddae77699482",
+    "url": "/static/js/2.c343a368.chunk.js"
+  },
+  {
+    "revision": "c81bed8303fd1206973a",
+    "url": "/static/js/main.38b409c6.chunk.js"
+  },
+  {
+    "revision": "06485921ec4767afd7f4",
+    "url": "/static/js/runtime-main.054f2952.js"
+  },
+  {
+    "revision": "01798bc13e33afc36a52f2826638d386",
+    "url": "/static/media/Pe-icon-7-stroke.01798bc1.ttf"
+  },
+  {
+    "revision": "71394c0c7ad6c1e7d5c77e8ac292fba5",
+    "url": "/static/media/Pe-icon-7-stroke.71394c0c.eot"
+  },
+  {
+    "revision": "799aa38b7f98f51e3f1d3d457d1151ef",
+    "url": "/static/media/Pe-icon-7-stroke.799aa38b.svg"
+  },
+  {
+    "revision": "b38ef310874bdd008ac14ef3db939032",
+    "url": "/static/media/Pe-icon-7-stroke.b38ef310.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8872104d67f7f8fcc83cc24ff5d9ebe3.js"
+  "/precache-manifest.4f048a12fbfecd1ed82e88ba41f9c640.js"
 );
 
 self.addEventListener('message', (event) => {
